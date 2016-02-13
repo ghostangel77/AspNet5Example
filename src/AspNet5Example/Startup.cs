7f7253627e91c233468c2db4c460c01f4701173e
ghostangel77 <ghostangel77@gmail.com>
2016-02-13 11:00:26 -0600

    Start MVC branch.

--- src/AspNet5Example/Startup.cs
+++ src/AspNet5Example/Startup.cs
@@ -18,54 +18,14 @@ namespace AspNet5Example
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddTransient<ISensorDataService, SensorDataService>();
+            services.AddMvc();
+            services.AddSingleton<ISensorDataService, SensorDataService>();
         }
-
-        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app)
-        //{
-        //    app.UseIISPlatformHandler();
-
-        //    app.Run(async (context) =>
-        //    {
-        //        await context.Response.WriteAsync("Hello World!");
-        //    });
-        //}
-
-        //public void Configure(IApplicationBuilder app)
-        //{
-        //    app.UseIISPlatformHandler();
-
-        //    app.Use(async (context, next) =>
-        //    {
-        //        await context.Response.WriteAsync("Hello World!");
-        //        await next();
-        //    });
-
-        //    app.Run(async (context) =>
-        //    {
-        //        await context.Response.WriteAsync("Hello World again!");
-        //    });
-        //}
 
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(minLevel: LogLevel.Information);
-            var logger = loggerFactory.CreateLogger("info");
-
             app.UseIISPlatformHandler();
 
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("Before next in first middleware");
-                await next();
-                logger.LogInformation("After next in first middleware");
-            });
-
-            app.Run(async (context) =>
-            {
-                logger.LogInformation("Second middleware");
-                await context.Response.WriteAsync("Hello World again!");
-            });
         }
 
         // Entry point for the application.
