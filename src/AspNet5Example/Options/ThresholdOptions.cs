a066ac094f6e1aad855b12d9a5e101d0d2a9fefe
Diego Torres <diego.torres@sterlingbackcheck.com>
2016-05-06 13:11:52 -0500

    ViewComponents

---
+++ src/AspNet5Example/Options/ThresholdOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet5Example.Options
+{
+    public class ThresholdOptions
+    {
+        public int FishMotionMax { get; set; }
+        public int FishMotionMin { get; set; }
+        public int LightIntensityMax { get; set; }
+        public int LightIntensityMin { get; set; }
+        public int WaterOpacityMax { get; set; }
+        public int WaterOpacityMin { get; set; }
+        public int WaterTemperatureMax { get; set; }
+        public int WaterTemperatureMin { get; set; }
+    }
+}
---
+++ src/AspNet5Example/ViewComponents/AlertViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using AspNet5Example.Options;
+using AspNet5Example.Services;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.OptionsModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet5Example.ViewComponents
+{
+    public class AlertViewComponent : ViewComponent
+    {
+        private ISensorDataService _sensorDataService;
+        private ThresholdOptions _thresholdConfig;
+
+        public AlertViewComponent(ISensorDataService sensorDataService, IOptions<ThresholdOptions> thresholdConfig)
+        {
+            this._sensorDataService = sensorDataService;
+            this._thresholdConfig = thresholdConfig.Value;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = new List<string>();
+
+            if (_sensorDataService.GetFishMotionPercentage().Value > _thresholdConfig.FishMotionMax)
+                viewModel.Add("Too much fish activity");
+            if (_sensorDataService.GetFishMotionPercentage().Value < _thresholdConfig.FishMotionMin)
+                viewModel.Add("Looks like we have some dead fish");
+
+            if (_sensorDataService.GetLightIntensityLumens().Value > _thresholdConfig.LightIntensityMax)
+                viewModel.Add("Bright light, bright light!");
+            if (_sensorDataService.GetLightIntensityLumens().Value < _thresholdConfig.LightIntensityMin)
+                viewModel.Add("It's dark out here");
+
+            if (_sensorDataService.GetWaterOpacityPercentage().Value > _thresholdConfig.WaterOpacityMax)
+                viewModel.Add("The fish can't see you");
+            if (_sensorDataService.GetWaterOpacityPercentage().Value < _thresholdConfig.WaterOpacityMin)
+                viewModel.Add("Water too clean");
+
+            if (_sensorDataService.GetWaterTemperatureFahrenheit().Value > _thresholdConfig.WaterTemperatureMax)
+                viewModel.Add("Water too hot!");
+            if (_sensorDataService.GetWaterTemperatureFahrenheit().Value < _thresholdConfig.WaterTemperatureMin)
+                viewModel.Add("Water too cold!");
+
+            return View(viewModel);
+        }
+    }
+}
